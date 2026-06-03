0d58d087c9abf6a1d32a30b2b460b11292a2d9a2
El-76 <maaaah@mail.ru>
2026-06-03 13:04:25 +0300

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> freq = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            Integer f = freq.get(c);
+            if (f == null) {
+                f = 1;
+            } else {
+                f++;
+            }
+            freq.put(c, f);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            Character c = t.charAt(i);
+            Integer f = freq.get(c);
+            if (f == null) {
+                return false;
+            } else if (f == 1) {
+                freq.remove(c);
+            } else {
+                freq.put(c, f - 1);
+            }
+        }
+
+        return freq.isEmpty();
+    }
+}
